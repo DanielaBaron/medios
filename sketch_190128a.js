cccd9b48c21a22ca884f2bc3d86c2ebc76ee68c2
DanielaBaron <47362708+DanielaBaron@users.noreply.github.com>
2019-02-05 17:21:36 -0500

    Add files via upload

---
+++ sketch_190128a.js
@@ -0,0 +1,24 @@
+ function setup() { 
+createCanvas(400,400);
+noStroke();
+//Fondo
+fill(227,227,231); 
+rect(0,0,400,400); 
+//Poligono amarillo 
+fill(222,191,36); 
+quad(170,258,170,360,272,275,220,225); 
+//Poligono amarillo/verde 
+fill(134,110,46,210); 
+quad(80,175,220,90,300,220,195,310);
+//Triangulo negro largo
+fill(0,0,0)
+triangle(38,330,44,342,372,40);
+//bola negra
+ellipse(240,130,60,60);
+//triangulo negro largo 2
+triangle(202,170,220,190,370,325)
+triangle(202,170,220,190,165,198)
+quad(
+} 
+
+  
